6fc47a86fbc37ab75031630be6ad8ade69f4d437
golubs <oleggolubs@gmail.com>
2020-03-23 05:27:38 +0100

    Add card-list

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './card-list.styles.css';
+
+const CardList = props => {
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
