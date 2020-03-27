de1dbab5e7846393a9ff090d1b9be901c2d61072
„golubs“ <oleggolubs@gmail.com>
2020-03-28 00:45:35 +0100

    Updates

---
+++ precache-manifest.2797c470e39664b3cdb6849ae06ac5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169d00feed5c4f7d09b2aa44e7241c8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c481d7ed745322ef5ff",
+    "url": "/monsters-rolodex/static/css/main.43dc78ad.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c481d7ed745322ef5ff",
+    "url": "/monsters-rolodex/static/js/main.8aaf9496.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
